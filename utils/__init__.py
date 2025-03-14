8f3604835cbd29ca61854ec7854a812f2d118cd4
ziyi105 <ahziyi@gmail.com>
2025-03-14 10:33:34 +0800

    Add Google Colab GPU usage case in utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -335,6 +335,11 @@ def save_on_master(*args, **kwargs):
 
 
 def init_distributed_mode(args):
+    if 'COLAB_GPU' in os.environ:  # Check if running in Colab
+        print("Running in Google Colab - Disabling distributed training")
+        args.distributed = False
+        args.gpu = 0  # Force single GPU usage
+        return
     if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
         args.rank = int(os.environ["RANK"])
         args.world_size = int(os.environ['WORLD_SIZE'])

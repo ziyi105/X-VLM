a00d71b48ec6fe42bce9a649181c270458a429c0
ziyi105 <ahziyi@gmail.com>
2025-03-27 01:07:57 +0800

    Enforce a fixed size for (w, h)

--- dataset/utils.py
+++ dataset/utils.py
@@ -338,8 +338,8 @@ def computeIoU(box1, box2):
     # each box is of [x1, y1, w, h]
     inter_x1 = max(box1[0], box2[0])
     inter_y1 = max(box1[1], box2[1])
-    inter_x2 = min(box1[0] + box1[2] - 1, box2[0] + box2[2] - 1)
-    inter_y2 = min(box1[1] + box1[3] - 1, box2[1] + box2[3] - 1)
+    inter_x2 = min(box1[0] + 8, box2[0] + 8)
+    inter_y2 = min(box1[1] + 8, box2[1] + 8)
 
     if inter_x1 < inter_x2 and inter_y1 < inter_y2:
         inter = (inter_x2 - inter_x1 + 1) * (inter_y2 - inter_y1 + 1)

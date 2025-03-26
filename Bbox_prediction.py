66eddd3db422dc3f0568c6e38246c7d0795853e2
ziyi105 <ahziyi@gmail.com>
2025-03-27 01:07:26 +0800

    Scale predicted bbox to match the size of original image

--- Bbox_prediction.py
+++ Bbox_prediction.py
@@ -6,13 +6,13 @@ from models.model_bbox import XVLM
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import yaml
-
+W, H = 384, 384
 # Load the model
 yaml_config_path = "configs/Bbox_test.yaml"  # Path to your YAML file
 with open(yaml_config_path, 'r') as f:
     config = yaml.safe_load(f)  # Load YAML as a Python dictionary
 
-checkpoint_path = 'content/drive/MyDrive/fyp-dataset/checkpoints/checkpoint_best.pth'
+checkpoint_path = '/content/drive/MyDrive/fyp-dataset/checkpoints/checkpoint_best.pth'
 
 model = XVLM(config=config)
 model.load_state_dict(torch.load(checkpoint_path)['model'])
@@ -31,7 +31,7 @@ def preprocess_image(image_path, image_res):
     image = (image - 0.5) / 0.5  # Normalize to [-1, 1]
     return image.unsqueeze(0)  # Add batch dimension
 
-image_path = '/content/drive/MyDrive/fyp-dataset/test_images/cropped_scene_210.jpeg'  # Replace with your image path
+image_path = '/content/drive/MyDrive/fyp-dataset/test_images/cropped_scene_150.jpeg'  # Replace with your image path
 image = preprocess_image(image_path, config['image_res']).to(device)
 
 # Preprocess the text
@@ -44,11 +44,17 @@ with torch.no_grad():
 
 # Get the predicted bounding box
 pred_bbox = outputs_coord[0].cpu().numpy()  # Convert to numpy array
-print("Predicted Bounding Box:", pred_bbox)
+pred_bbox_abs = [
+    int(pred_bbox[0] * W),  # x_min
+    int(pred_bbox[1] * H),  # y_min
+    int(pred_bbox[2] * W),  # x_max
+    int(pred_bbox[3] * H)   # y_max
+]
+print("Predicted Bounding Box:", pred_bbox_abs)
 
-x_min, y_min, width, height = pred_bbox
-x_max = x_min + width
-y_max = y_min + height
+x_min, y_min, width, height = pred_bbox_abs
+x_max = x_min + 14
+y_max = y_min + 14
 
 # Load the original image
 original_image = Image.open(image_path).convert('RGB')
@@ -58,7 +64,7 @@ fig, ax = plt.subplots(1, figsize=(8, 8))
 ax.imshow(original_image)
 
 # Add the bounding box as a rectangle
-rect = patches.Rectangle((x_min, y_min), width, height, linewidth=2, edgecolor='r', facecolor='none')
+rect = patches.Rectangle((x_min, y_min), 14, 14, linewidth=2, edgecolor='r', facecolor='none')
 ax.add_patch(rect)
 
 # Add a label for the bounding box
@@ -66,3 +72,6 @@ ax.text(x_min, y_min - 10, "Predicted BBox", color='red', fontsize=12, weight='b
 
 plt.axis('off')  # Turn off the axis
 plt.show()
+# Save the plot to a file
+output_path = "output_bbox.png"
+fig.savefig(output_path)
